d7f3493eed45f6413676e069285438bb5327fff1
Artiom Divak <artten12380@gmail.com>
2021-10-17 17:27:43 +0300

    trying

--- ex1/anomaly_detection_util.cpp
+++ ex1/anomaly_detection_util.cpp
@@ -1,3 +1,10 @@
+/*
+ * animaly_detection_util.cpp
+ *
+ * Author: 319339198 Artiom Divak
+ * 316440023 Saar Aloni
+ */
+
 #include "anomaly_detection_util.h"
 #include <iostream>
 #include <cmath>
@@ -31,28 +38,29 @@ void multipileTwoArrays (T* arryOne, T* arryTwo, T* newArry, int size) {
 
 // returns the variance of X and Y
 float var(float* x, int size) {
-  float n = 1/size;
+  float n = 1/(float) size;
   float sum = sumOfArray<float>(x, size);
   float expetation = n * sum;
   float arry[size];
   for (int i = 0; i < size; i++){
     arry[i] = (x[i] - expetation) * (x[i] - expetation);
   }
-  float var = n * sumOfArray<float>(arry, size);
-  return var;
+  float result = n * sumOfArray<float>(arry, size);
+  return result;
 }
 
 // returns the covariance of X and Y
 float cov(float* x, float* y, int size) {
-  float varOfX = var(x, size);
-  float varOfY = var(y, size);
+  float n = 1/(float) size;
+  float expetationOfX = sumOfArray<float>(x, size) * n;
+  float expetationOfY = sumOfArray<float>(y, size) * n;
   float xMinusExpetation[size];
   float yMinusExpetation[size];
-  addToAllArrayVeribals(x, xMinusExpetation, -varOfX, size);
-  addToAllArrayVeribals(y, yMinusExpetation, -varOfY, size);
+  addToAllArrayVeribals(x, xMinusExpetation, -expetationOfX, size);
+  addToAllArrayVeribals(y, yMinusExpetation, -expetationOfY, size);
   float finalArry[size];
   multipileTwoArrays(xMinusExpetation, yMinusExpetation, finalArry, size);
-  float result = var(finalArry, size);
+  float result = sumOfArray<float>(finalArry, size) * n;
   return result;
 }
 
@@ -73,25 +81,29 @@ float pearson(float* x, float* y, int size) {
 ***/
 Line linear_reg(Point** points, int size) {
 
-    float sumX = 0;
-    float sumY = 0;
-    float avgX = 0;
-    float avgY = 0;
-    float a = 0;
+  float sumX = 0;
+  float sumY = 0;
+  float avgX = 0;
+  float avgY = 0;
+  float a = 0;
+  float arrayX[size];
+  float arrayY[size];
+  for (int i = 0; i < size; i++) {
+      sumX += points[i]->x;
+      arrayX[i] = points[i]->x;
+      sumY += points[i]->y;
+      arrayY[i] = points[i]->y;
+  }
+  avgX = sumX / size;
+  avgY = sumY / size;
 
-    for (int i = 0; i < size; i++) {
-        sumX += points[i]->x;
-        sumY += points[i]->y;
-    }
-    avgX = sumX / size;
-    avgY = sumY / size;
 
-    a = cov(&avgX, &avgY, size) / var(&avgX, size);
-    Line l;
-    l.a = a;
-    l.b = avgY - (a * avgX);
-    return l;
-    }
+  a = cov(arrayX, arrayY, size) / var(arrayX, size);
+  Line l;
+  l.a = a;
+  l.b = avgY - (a * avgX);
+  return l;
+}
 
 /*
 * Function Name: dev
@@ -99,10 +111,10 @@ Line linear_reg(Point** points, int size) {
 * Output: the devation
 * Function Operation: returns the deviation between point p and the line equation of the points
 ***/
-    float dev(Point p, Point points, int size) {
-        Line l = linear_reg(points, size);
-        return abs(p.y - l.f(p.x));
-    }
+float dev(Point p, Point** points, int size) {
+    Line l = linear_reg(points, size);
+    return abs(p.y - l.f(p.x));
+}
 
 /***
 * Function Name: dev
@@ -110,6 +122,6 @@ Line linear_reg(Point** points, int size) {
 * Output: the devation
 * Function Operation: returns the deviation between point p and the line
 */
-    float dev(Point p, Line l) {
-        return abs(p.y - l.f(p.x));
-    }
+float dev(Point p, Line l) {
+  return abs(p.y - l.f(p.x));
+}
