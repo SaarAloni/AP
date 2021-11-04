349d09c9a9afb5ac7a4c6b882eebff57f3226d2a
Artiom Divak <artten12380@gmail.com>
2021-11-04 18:51:11 +0200

    starting ex2

---
+++ ex2/timeseries.cpp
@@ -0,0 +1,21 @@
+class TimeSeries{
+
+int ** columns;
+int length;
+
+public:
+
+	TimeSeries(const char* CSVfileName){
+
+	}
+
+  int getLength() {
+    return length;
+  }
+
+  int * getColumn(int index) {
+
+  }
+
+
+}
