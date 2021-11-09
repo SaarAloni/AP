879d49eeb462cf314e1595d30adc3eb5b865404e
Saar Aloni <alonisaar@gmail.com>
2021-11-09 14:49:26 +0200

    learn normal

--- ex2/SimpleAnomalyDetector.cpp
+++ ex2/SimpleAnomalyDetector.cpp
@@ -1,10 +1,10 @@
 
 #include "SimpleAnomalyDetector.h"
 #include "timeseries.h"
+#include <cmath>
 
 SimpleAnomalyDetector::SimpleAnomalyDetector() {
 	// TODO Auto-generated constructor stub
-
 }
 
 SimpleAnomalyDetector::~SimpleAnomalyDetector() {
@@ -14,28 +14,35 @@ SimpleAnomalyDetector::~SimpleAnomalyDetector() {
 
 void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
 
-  float threshold = 0.75;
-
-      TimeSeries t = ts;
-      pair<int, float> result[ts.getColumnLength()];
       for (int i = 0; i < ts.getColumnLength(); i++) {
           float p = 0;
-          if (result[i] != NULL) {
-              p = result[i].second;
-          }
+          float* a = &(ts.getColumnByIndex(i).second[0]);
           float core = 0;
+          float max = 0;
           int index = -1;
           for (int j = i + 1; j <  ts.getColumnLength(); j++) {
-              if (p = pearson(ts.getColumnByIndex(i).second, ts.getColumnByIndex(j).second, ts.getColumnLength()) > core && p > threshold) {
+              const float* a = &(ts.getColumnByIndex(i).second[0]);
+              const float* b = &(ts.getColumnByIndex(j).second[0]);
+              p = abs(pearson(a, b, ts.getColumnLength()));
+              if (p > core) {
                   core = p;
                   index = j;
               }
           }
           if (index != -1) {
-              result[i].first = index;
-              result[i].second = p;
-              result[index].first = i;
+              this->cf[i].corrlation = core;
+              this->cf[i].feature1 = ts.getColumnByIndex(i).first;
+              this->cf[i].feature2 = ts.getColumnByIndex(index).first;
+              this->cf[i].lin_reg = linear_reg((a, a), ts.getRowLength());
 
+              for (int j = 0; j < ts.getRowLength(); j++) {
+                  Point point = Point(ts.getColumnByIndex(i).second[j], ts.getColumnByIndex(index).second[j]);
+                  float devation = dev(point, this->cf[i].lin_reg);
+                  if (devation > max) {
+                      max = devation;
+                  }
+              }
+              this->cf[i].threshold = max * 1.1;
           }
       }
 	// TODO Auto-generated destructor stub
