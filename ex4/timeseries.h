03cfa924a2b641f54ecf96e08ff717a369128e70
Artiom Divak <artten12380@gmail.com>
2021-12-11 11:00:58 +0200

    ex4

---
+++ ex4/timeseries.h
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+
+#ifndef TIMESERIES_H_
+#define TIMESERIES_H_
+
+using namespace std;
+
+class TimeSeries{
+
+	std::vector<std::pair<std::string, std::vector<float>>> result = {};
+	int colLength = 0;
+	int rowLength = 0;
+
+public:
+
+	TimeSeries(const char* CSVfileName);
+	int getRowLength() const;
+	int getColumnLength() const;
+	std::pair<std::string, std::vector<float>> getColumnByIndex(int index) const;
+	std::vector<float> getColumnByName(std::string name) const;
+	float getColumnByIndex(int column, int row) const;
+
+};
+
+
+
+#endif /* TIMESERIES_H_ */
